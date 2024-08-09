ba67cfc4e2134e4936f95fca4d599e3df3c176be
deanmastria <dean.mastria@gmail.com>
2024-08-09 13:41:08 -0400

    task 3 complete, modified buffered i/o observations noted in ReadMe file

---
+++ src/main/java/org/example/ModifiedBufferWriter.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ModifiedBufferWriter {
+    public static void main(String[] args) {
+        String fileName = "src/main/resources/modifiedtask1.txt";                       // Specifies the name of the file to write
+        String data = "String for task 3";                                      // The string data to be written
+
+        try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(fileName))) {           // Try-wit-resources to ensure FileOutputStream is closed automaitcally
+            bos.write(data.getBytes());                                         // Converts the string to bytes and writes it to the file
+            System.out.printf("Data written to file '%s'\n", fileName);         // Output to console to inform user the file was written successfully
+        } catch (IOException e) {
+            e.printStackTrace();                                                // Handle any IOExceptions
+        }
+    }
+}
---
+++ src/main/java/org/example/ModifiedBufferedReader.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ModifiedBufferedReader {
+    public static void main(String[] args) {
+        String fileName = "src/main/resources/modifiedtask1.txt";                   // Name of file to be read
+
+        try (BufferedInputStream bis = new BufferedInputStream( new FileInputStream(fileName))) {         // Try-with-resources to ensure that FileInputStream is closed automatically
+            int byteData;
+
+            while ((byteData = bis.read()) != -1) {                         //Read each byte from the file until the end is reached
+                System.out.println((char) byteData);                        // Convert the byte to characters and print
+            }
+        }catch (IOException e) {
+            e.printStackTrace();                                            // Handles any IOExceptions if they occur
+        }
+
+    }
+}
+
